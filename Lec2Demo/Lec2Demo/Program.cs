22d775d356c8aee73f39ae22312874f1e3f43cb5
Lyra Ganem <lyraganemt@student.douglascollege.ca>
2023-01-11 10:19:01 -0800

    adding writelines

---
+++ Lec2Demo/Lec2Demo/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Lec2Demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("************************");
+            WriteLine("*   Name: Thiago       *");
+            WriteLine("*   Faculty: CBA       *");
+            WriteLine("*   Dept: CSIS         *");
+            WriteLine("************************");
+        }
+    }
+}
