eaa05d09e7950f46cead18b2e0167dc693077601
Lyra Ganem <lyraganemt@student.douglascollege.ca>
2023-02-01 09:09:08 -0800

    Adding lab 5 classes

---
+++ Lec5_1Feb_LabFiles/TextBookExamples/Chapter4Examples/StudentApp/StudentApp.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using static System.Console;
+
+namespace StudentApp
+{
+    class StudentApp
+    {
+        static void Main(string[] args)
+        {
+            Student firstStudentObject = new Student();
+            firstStudentObject.StudentFirstName = AskForStudentName("First");
+			firstStudentObject.StudentLastName = AskForStudentName("Last");
+			firstStudentObject.StudentNumber = AskForStudentNumber();
+            firstStudentObject.Major = AskForMajor(firstStudentObject.StudentFirstName);
+            firstStudentObject.Score1 = AskForExamScore(1);
+            firstStudentObject.Score2 = AskForExamScore(2);
+            firstStudentObject.Score3 = AskForExamScore(3);
+
+            Clear();
+            WriteLine("First Student");
+            WriteLine(firstStudentObject.ToString());
+
+
+            Student secondStudentObject = new Student("2345");
+            secondStudentObject.Score1 = 95;
+            secondStudentObject.Score2 = 62;
+            secondStudentObject.Score3 = 87;
+            WriteLine("\n\nSecond Student");
+            WriteLine("Student Number: " + secondStudentObject.StudentNumber +
+                               "\nAverage: {0:F1}", secondStudentObject.CalculateAverage());
+
+
+            Student thirdStudentObject = new Student("5432", "Randolph", "Wonder");
+            thirdStudentObject.Major = "Math";
+            thirdStudentObject.Score1 = 95;
+            thirdStudentObject.Score2 = 87;
+            thirdStudentObject.Score3 = 72;
+            WriteLine("\n\nThird Student");
+            WriteLine(thirdStudentObject);
+
+
+            Student aStudentObject = new Student("1234", "Maria", "Smith", 97, 75, 87,"CS");
+            WriteLine("\n\nLast Student");
+            WriteLine("Student Name: " + aStudentObject.StudentFirstName +
+                              " " + aStudentObject.StudentLastName +
+                              "\nStudent Number: " + aStudentObject.StudentNumber +
+                              "\nMajor: " + aStudentObject.Major +
+                              "\nExam Score 1: " + aStudentObject.Score1 +
+                              "\nExam Score 2: " + aStudentObject.Score2 +
+                              "\nExam Score 3: " + aStudentObject.Score3 +
+                              "\nExam Average: " + aStudentObject.CalculateAverage().ToString("F1"));
+            ReadKey();
+        }
+
+        static int AskForExamScore(int whichOne)
+        {
+            string inValue;
+            int aScore;
+            Write("Enter a value for Score {0}: ", whichOne);
+            inValue = ReadLine();
+            aScore = int.Parse(inValue);
+            return aScore;
+        }
+
+        static string AskForStudentName(string whichOne)
+        {
+            string inValue;
+            Write("Enter Student {0} Name: ", whichOne);
+            inValue = ReadLine();
+            return inValue;
+        }
+        static string AskForMajor(string name)
+        {
+            string inValue;
+            Write("Enter {0}\'s Major: ", name);
+            inValue = ReadLine();
+            return inValue;
+        }
+        static string AskForStudentNumber()
+        {
+            string inValue;
+            Write("Enter Student Number: ");
+            inValue = ReadLine();
+            return inValue;
+        }
+    }
+}
---
+++ Lec5_1Feb_LabFiles/TextBookExamples/Chapter5Examples/StatePicker/StatePicker/StatePicker.cs
@@ -0,0 +1,59 @@
+﻿/* StatePicker.cs		Author:	Doyle	
+ * Allows the user to enter a state
+ * abbreviation. A switch statement 
+ * is used to display the full name
+ * of the state. 
+ * *********************************/
+using System;
+using static System.Console;
+
+namespace StatePicker
+{
+    class StatePicker
+    {
+        static void Main( )
+        {
+            string stateAbbrev;
+
+            WriteLine("Enter the state abbreviation. ");
+            WriteLine("Its full name will be displayed.");
+            WriteLine();
+            stateAbbrev = ReadLine();
+
+            switch (stateAbbrev)
+            {
+                case "AL":
+                case "aL":
+                case "Al":
+                case "al":
+                    WriteLine("Alabama");
+                    break;
+                case "FL":
+                    WriteLine("Florida");
+                    break;
+                case "GA":
+                    WriteLine("Georgia");
+                    break;
+                case "IL":
+                    WriteLine("Illinois");
+                    break;
+                case "KY":
+                    WriteLine("Kentucky");
+                    break;
+                case "MI":
+                    WriteLine("Michigan");
+                    break;
+                case "OK":
+                    WriteLine("Oklahoma");
+                    break;
+                case "TX":
+                    WriteLine("Texas");
+                    break;
+                default:
+                    WriteLine("No match");
+                    break;
+            }
+            Console.ReadKey();
+        }
+    }
+}
