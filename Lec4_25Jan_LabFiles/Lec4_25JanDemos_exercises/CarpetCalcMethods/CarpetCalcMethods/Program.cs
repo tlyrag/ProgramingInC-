4637f700743c19edfa341a31914bcb43863ff827
Thiago Ganem <tlyraganem@gmail.com>
2023-02-01 13:30:07 -0800

    lec4 exercises

---
+++ Lec4_25Jan_LabFiles/Lec4_25JanDemos_exercises/CarpetCalcMethods/CarpetCalcMethods/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace CarpetCalcMethods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double lengthInFt = GetDimension("length");
+            double widthInFt = GetDimension("width");
+            WriteLine("Total length = {0:F2}, Total width = {1:F2}",
+                                                lengthInFt,widthInFt);
+            double areaInSqYds = GetAreaInSqYds(lengthInFt, widthInFt);
+            WriteLine("Area in sq. yds = {0:F2}", areaInSqYds);
+            GetCarpetPrices("berber","pile",
+                  out double berberPrice, out double pilePrice);
+            WriteLine("Berber price = {0:C} per sq. yd, " +
+                "Pile price = {1:C} per sq. yd", 
+                                berberPrice, pilePrice);
+            double berberCost = GetCarpetCost(berberPrice, areaInSqYds);
+            double pileCost = GetCarpetCost(pilePrice, areaInSqYds);
+            WriteLine("Berber cost = {0:C}, " +
+                "Pile cost = {1:C}",
+                                berberCost, pileCost);
+            //Practice Exercise: After completing 
+            //DisplayBanner() method definition below.
+            //Call it with needed input arguments
+            //DisplayBanner(lengthInFt,widthInFt,
+            //    berberPrice,pilePrice,berberCost,pileCost)
+
+        }
+
+        //method definition - outside all other methods, inside program class
+
+        //Display banner method definition
+        //Practice Exercise: See the method definition below
+        //DisplayBanner is similar to DisplayBanner in the Lec4MethodsDemo
+        //It is a void method that takes as input lengthFt, widthFt,
+        //berberPrice, pilePrice,berberCost, pileCost, 
+        //displays formatted output in banner form
+        static void DisplayBanner(double lengthFt, double widthFt,
+                                    double berberPrice, double pilePrice,
+                                       double berberCost, double pileCost)
+        {
+            WriteLine(new string('*', 50));
+            WriteLine("*{0, 16}: {1, -8:F1}{2, -22}*", "Length", lengthFt, "Feet");
+            //Practice exercise - complete this banner - similar to Lec4Methods demo Display banner
+            //and using the format given above, complete the banner to display
+            //widthFt, berberPrice, pilePrice, berberCost, pileCost
+
+            //Don't forget to call this method in the main method to see the result
+        }
+
+
+        //write a value returning method,
+        //GetCarpetCost() that returns the double cost given 
+        //carpetPrice and areaInSqYds
+        static double GetCarpetCost(double carpetPrice, double areaInSqYds)
+        {
+            return carpetPrice * areaInSqYds;
+        }
+        static void GetCarpetPrices(string carpetType1, 
+                                    string carpetType2,
+                                    out double price1, 
+                                    out double price2)
+        {
+            Write("Enter the price for " + carpetType1 + ": ");
+            price1 = double.Parse(ReadLine());
+            Write("Enter the price for " + carpetType2 + ": ");
+            price2 = double.Parse(ReadLine());
+        }
+        static double GetAreaInSqYds(double length, double width)
+        {
+            const int SQFT_PER_SQYD = 9;
+            //SQFT_PER_SQYD = 20; //constants cannot be changed
+            double sqYds = (length * width) / SQFT_PER_SQYD;
+            return sqYds;
+        }
+        static double GetDimension(string dimension)
+        {
+            //double totalDimension;
+            Write("Enter the " + dimension + " in feet: ");
+            int feet = int.Parse(ReadLine());
+            Write("Enter the " + dimension + " in inches: ");
+            int inches = int.Parse(ReadLine());
+            double totalDimension = feet + (double)inches/12;
+            return totalDimension;
+        }
+    }
+}
---
+++ Lec4_25Jan_LabFiles/Lec4_25JanDemos_exercises/Lec4MethodsDemo/Lec4MethodsDemo/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Lec4MethodsDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Method demo 1: void return type, no input method");
+            SayHello();
+            SayHello();
+            SayHello();
+            //WriteLine(helloMsg); //will not work because helloMsg declared inside the method
+
+            WriteLine("Method demo 2: void return type, two string inputs");
+            SayHelloWithInputs("Hello", "Amanda");
+            string anyMsg = "Welcome";
+            string name = "Edward";
+            SayHelloWithInputs(anyMsg, name); //arguments - may be literals or variables
+            //input parameter => value of the argument goes into the parameter
+            WriteLine(name); //argument is available main method
+            //WriteLine(userName); //parameter from method cannot be accessed outside
+
+            WriteLine("Method demo 3: Value-returning, no input method");
+            string customerName = GetNameFromUser();
+            SayHelloWithInputs("Have a good day", customerName);
+
+            SayHelloWithInputs("Hi ", customerName + "! Welcome to the high grade calculator");
+            //out arguments - must be variables
+            double gradeOne;
+            GetTwoGrades(customerName, out gradeOne, 
+                                        out double gradeTwo);
+            WriteLine("Grades are {0} and {1}",gradeOne,gradeTwo);
+
+            double maxGrade = GetMax(gradeOne, gradeTwo);
+
+            DisplayBanner(customerName, gradeOne, gradeTwo, maxGrade);
+
+            //Practice Exercise: Create/define a method (outside
+            //the main method) that is void method with string input
+            //parameter and out string parameter 
+            //GetName(string nameType, out string name)
+            //This method takes as input nameType either "first"
+            //or "last" and then asks user for Enter first/last name
+            //...sets the out parameter for first/last
+            //You will then call this method in the main method
+            //twice for getting out first and last name
+            //Then display it as "Last name", "First Name"
+
+        }
+        static void SayHello()
+        {
+            string helloMsg = "Hi, Priya!"; //variable is local to this method
+            WriteLine(helloMsg);
+        }
+        static void SayHelloWithInputs(string msg,
+                                        string userName)
+        {
+            //parameters only available inside the method definition
+            //and is used everytime the method is called
+            //to execute the method based on the input parameters
+            WriteLine(msg + "**********" + userName);
+        }
+
+        static string GetNameFromUser() //no input parameters, string value returning method
+        {
+            Write("Enter the user's name: ");
+            string userName = ReadLine(); //userName - local variable
+            return userName; //control leaves the method
+           // WriteLine("This is the end"); //unreachable code because it is after return statement
+        }
+
+        static void GetTwoGrades(string userName, out double grade1, 
+                                                out double grade2)
+        {
+            Write("Enter the grade1 for " + userName + ": ");
+            grade1 = double.Parse(ReadLine());
+            Write("Enter the grade2 for " + userName + ": ");
+            grade2 = double.Parse(ReadLine());
+        }
+
+        static double GetMax(double grade1, double grade2)
+        {
+            return Math.Max(grade1, grade2);
+        }
+
+        private static void DisplayBanner(string customerName, 
+            double grade1, double grade2, double maxGrade)
+        {
+            string asterikLine = new string('*', 44);
+            WriteLine(asterikLine);
+            WriteLine("*{0, 15}: {1, -25}*", "User Name", customerName);
+            WriteLine("*{0, 15}: {1, 5:F1}, {2, -18:F1}*", "Grades", grade1, grade2);
+            WriteLine("*{0, 15}: {1, -25:F1}*", "Highest Grade", maxGrade);
+            WriteLine(asterikLine);
+        }
+
+    }
+}
