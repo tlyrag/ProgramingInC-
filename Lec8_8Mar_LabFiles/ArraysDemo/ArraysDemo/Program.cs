af360ef6aa1f9a343d35610781f7855a942f1283
Lyra Ganem <lyraganemt@student.douglascollege.ca>
2023-03-08 10:10:13 -0800

    adding lec8

---
+++ Lec8_8Mar_LabFiles/ArraysDemo/ArraysDemo/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace ArraysDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            WriteLine("Demo 1: Initializing and displaying an integer array");
+            int[] numbersArray = new int[] { 78, 89, 77, 23, 98 };
+            int numberOfElements = numbersArray.Length;
+            for (int i = 0; i < numberOfElements; i++)
+            {
+                Write(numbersArray[i] + "\t");
+            }
+            WriteLine();
+
+            WriteLine("Demo 2: string array");
+            string[] namesArray = { "John", "Amy", "William" };
+            WriteLine("Displaying string array elements...");
+
+            for (int i = 0; i < namesArray.Length; i++)
+            {
+                WriteLine(namesArray[i]);
+            }
+
+            DisplayArray(namesArray);
+        }
+
+        static void DisplayArray(string[] anyArray)
+        {
+            WriteLine("Inside Display Array method using array as parameter....");
+            for (int i = 0; i < anyArray.Length; i++)
+            {
+                WriteLine(anyArray[i]);
+            }
+            WriteLine();
+        }
+
+
+
+    }
+}
---
+++ Lec8_8Mar_LabFiles/ForLoopsDemo/ForLoopsDemo/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace ForLoopsDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Demo 1: Times table using for with increasing counter");
+            for (int i = 1; i <= 12; i++)
+            {
+                WriteLine("Entered loop " + i);
+                WriteLine(i + "\t" + i * 6);
+            }
+
+            WriteLine("Demo 2: Times table using for with decreasing order " +
+                                "and concatenated string output");
+            string outputStr = "";
+            for (int i = 12; i > 0; i--)
+            {
+                WriteLine("Entered loop " + i);
+                outputStr += i + "\t" + i * 6 + "\n"; //string concatenation
+            }
+            WriteLine(outputStr); //Displaying the combined string after loop is done
+
+
+            WriteLine("Demo 3: Times table until product limit is reached");
+            int product = 0;
+            for (int numCount = 1; product < 100; numCount++)
+            {
+                product = numCount * 6;
+                string outputStr1 = numCount + "\t" + product; //note here it is not string concatenation
+                WriteLine(outputStr1);
+            }
+
+        }
+    }
+}
