4e786cc2b27fab1df576d19289eefdbeed1f9327
Lyra Ganem <lyraganemt@student.douglascollege.ca>
2023-03-08 11:58:25 -0800

    adding demo3

--- Lec8_8Mar_LabFiles/ArraysDemo/ArraysDemo/Program.cs
+++ Lec8_8Mar_LabFiles/ArraysDemo/ArraysDemo/Program.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using static System.Console;
 
@@ -11,29 +13,51 @@ namespace ArraysDemo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            WriteLine("Demo 1: Initializing and displaying an integer array");
-            int[] numbersArray = new int[] { 78, 89, 77, 23, 98 };
-            int numberOfElements = numbersArray.Length;
-            for (int i = 0; i < numberOfElements; i++)
+            //Console.WriteLine("Hello World!");
+            //WriteLine("Demo 1: Initializing and displaying an integer array");
+            //int[] numbersArray = new int[] { 78, 89, 77, 23, 98 };
+            //int numberOfElements = numbersArray.Length;
+            //for (int i = 0; i < numberOfElements; i++)
+            //{
+            //    Write(numbersArray[i] + "\t");
+            //}
+            
+            //WriteLine();
+            //foreach(int a in numbersArray)
+            //{
+            //    Write(a + "\t");
+            //}
+
+            //WriteLine("Demo 2: string array");
+            //string[] namesArray = { "John", "Amy", "William" };
+            
+            //WriteLine("Displaying string array elements...");
+
+            //for (int i = 0; i < namesArray.Length; i++)
+            //{
+            //    //WriteLine(namesArray[i]);
+            //    Write("Enter Name " + i + 1 + " :");
+            //    namesArray[i] = ReadLine();
+            //}
+
+            //DisplayArray(namesArray);
+
+            WriteLine("Demo 3: working with double[], user input, validations");
+            int numGrades;
+            Write("How many grades do you have: ");
+            while(!int.TryParse(ReadLine(),out numGrades) || numGrades <=0)
             {
-                Write(numbersArray[i] + "\t");
+                Write("Number of grades mus be whole number >0, Please re-enter: ");
             }
-            WriteLine();
+            double[] gradeArray = new double[numGrades];
+            
+            LoadGrades(gradeArray);
+           // Write(gradeArray); // simply display data type, cannot display the array directly
+            DisplayArray(gradeArray);
 
-            WriteLine("Demo 2: string array");
-            string[] namesArray = { "John", "Amy", "William" };
-            WriteLine("Displaying string array elements...");
-
-            for (int i = 0; i < namesArray.Length; i++)
-            {
-                WriteLine(namesArray[i]);
-            }
-
-            DisplayArray(namesArray);
         }
 
-        static void DisplayArray(string[] anyArray)
+        static void DisplayArray(params string[] anyArray)
         {
             WriteLine("Inside Display Array method using array as parameter....");
             for (int i = 0; i < anyArray.Length; i++)
@@ -42,6 +66,35 @@ namespace ArraysDemo
             }
             WriteLine();
         }
+
+        static void DisplayArray(params double[] anyArray)
+        {
+            WriteLine("Inside Display Array method using array as parameter....");
+            foreach(double arg in anyArray)
+            {
+                WriteLine(arg);
+            }
+            WriteLine();
+        }
+
+        static void LoadGrades(double[] anyGrades)
+        {
+            WriteLine("Let us enter the grade");
+            for (int i = 0; i < anyGrades.Length; i++)
+            {
+                double grade;
+                Write($"Please enter the grade for student {i+1}: ");
+                while (!double.TryParse(ReadLine(), out grade) || grade<0 || grade>100)
+                {
+                    Write($"Please re-enter the grade for student {i + 1}: ");
+                }
+                anyGrades[i] = grade;
+                  
+
+                
+            }
+        }
+
 
 
 
---
+++ Lec8_8Mar_LabFiles/ArraysDemo/ArraysDemo/Ticket.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArraysDemo
+{
+    internal class Ticket
+    {
+        public int Id { get; set; }
+    }
+}
