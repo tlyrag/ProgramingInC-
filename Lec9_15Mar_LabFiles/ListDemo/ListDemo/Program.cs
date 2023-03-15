b90c0958b0c340a343af526a669e5f439a7c72c4
Lyra Ganem <lyraganemt@student.douglascollege.ca>
2023-03-15 11:50:04 -0700

    Adding list examples

--- Lec9_15Mar_LabFiles/ListDemo/ListDemo/Program.cs
+++ Lec9_15Mar_LabFiles/ListDemo/ListDemo/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,15 +12,23 @@ namespace ListDemo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            WriteLine("Hello World!");
             List<string> NamesList = new List<string>(); //constructing an empty list of string with 0 elements to start with
             WriteLine("Total number of names in the list before adding the names = " + NamesList.Count);
-
+            // add = push
+            
             NamesList.Add("John"); //this adds John to the end of the list
             NamesList.Add("William");
             NamesList.Add("Jim");
             NamesList.Add("Amy");
+            NamesList.Insert(1, "Marie");
+            NamesList.Insert(2, "Jim");
 
+            NamesList.Remove("Jim");
+            NamesList.RemoveAt(2);
+
+
+            WriteLine(NamesList);
             //Note that List has Count property but arrays have Length property
             WriteLine("Total number of names in the list = " + NamesList.Count);
 
@@ -28,7 +37,50 @@ namespace ListDemo
                 Write(name + "\t");
             }
             WriteLine();
+            List<double> Numbers = new List<double>() { 2.3,-5,-7,0,1,4,3,2 };
+            Numbers.Remove(0);
+            foreach(double num in Numbers)
+            {
+                WriteLine(num);
+            }
 
+            List<double> GradeList = new List<double>();
+
+            WriteLine($"Names count: {NamesList.Count}, Grades Count:{GradeList.Count}");
+            for(int i= 0; i< NamesList.Count; i++)
+            {
+                double grade;
+                Write($"Enter the grade for {NamesList[i]}: ");
+                while(!double.TryParse(ReadLine(),out grade) || grade<0 || grade>100)
+                {
+                    Write($"Please re enter grade for {NamesList[i]}:");
+                }
+                GradeList.Add(grade);
+                    
+            }
+            WriteLine($"Names count: {NamesList.Count}, Grades Count:{GradeList.Count}");
+            DisplayNamesAndGrade(NamesList, GradeList);
+            WriteLine($"The biggest grade was from: {GetNameOfMaxGrade(NamesList, GradeList)}");
+        }
+        static string GetNameOfMaxGrade(List<string> names, List<double> grades)
+        {
+            int maxGradeIndex = 0;
+            for(int i= 0; i< names.Count; i++)
+            {
+                if (grades[maxGradeIndex] < grades[i])
+                {
+                    maxGradeIndex=i;
+                }
+            }
+            string maxName = names[maxGradeIndex];
+            return maxName;
+        }
+        static void DisplayNamesAndGrade (List<string> names,List<double> grades)
+        {
+            for (int i = 0; i < names.Count; i++)
+            {
+                WriteLine($"{names[i]} got {grades[i]}");
+            }
         }
     }
 }
