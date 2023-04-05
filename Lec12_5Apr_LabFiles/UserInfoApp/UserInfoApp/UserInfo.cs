0369a0d387fe6a511e161602b0dcc2f0a76faa0f
Lyra Ganem <lyraganemt@student.douglascollege.ca>
2023-04-05 09:59:27 -0700

    Adding lab 12

---
+++ Lec12_5Apr_LabFiles/UserInfoApp/UserInfoApp/UserInfo.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserInfoApp
+{
+    internal class UserInfo
+    {
+        public string UserName { get; set; }
+        public int UserId { get; set; }
+        public double UserAge { get; set; }
+        public string UserCategory
+        {
+            get
+            {
+                if (UserAge < 18)
+                {
+                    return "Child";
+                } else if (UserAge >= 60)
+                {
+                    return "Senior Citizen";
+                } else
+                {
+                    return "Adult";
+                }
+            }
+        }
+
+        public UserInfo(string userName, int userId, double userAge)
+        {
+            UserName = userName;
+            UserId = userId;
+            UserAge = userAge;
+        }
+
+        public override string ToString()
+        {
+            string outputStr = string.Format("{0,-15}{1,-5}{2,-8:F2}{3,-10}", 
+                                    UserName, UserId, UserAge, UserCategory);
+            return outputStr;
+        }
+
+    }
+}
